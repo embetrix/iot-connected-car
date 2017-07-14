5457b5f168517d943cc0536a925becbb49892aac
Ayoub Zaki <ayoub.zaki@embexus.com>
2017-07-14 13:32:15 +0200

    add python files

---
+++ iot-connected-car.py
@@ -0,0 +1,34 @@
+# (C) Copyright 2017
+#
+# Embexus Embedded Systems Solutions, ayoub.zaki@embexus.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307 USA
+#
+
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+import settings as s
+
+pnconfig = PNConfiguration()
+
+pnconfig.publish_key = s.pubkey
+pnconfig.subscribe_key = s.subkey
+
+Data = [{'a': 1828292992, 'b': 727277272}]
+ 
+pubnub = PubNub(pnconfig)
+
+pubnub.publish().channel(s.channel).message(Data).sync()
